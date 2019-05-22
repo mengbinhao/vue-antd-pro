aa7801eab8408930d56c9a51042d2b31ca0c7aae
Jack <mengbinhao2018@gmail.com>
2019-05-22 14:25:47 +0800

    使用render完成user router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import RenderRouterView from './components/RenderRouterView'
 
 Vue.use(Router)
 
@@ -12,7 +11,7 @@ export default new Router({
 			path: '/user',
 			//need a <router-view></router-view> tag
 			//底层是都转换成render funcion
-			component: RenderRouterView,
+			component: { render: h => h('router-view') },
 			children: [
 				{
 					path: '/user/login',
